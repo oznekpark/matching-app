6913e9e51bf1dc80852531e7a57d51b6eb8e1db7
oznekpark <kgog.33@gmail.com>
2020-07-22 19:52:01 +0900

    jTinderの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
 end
